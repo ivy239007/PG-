4f15e12317699e92e20ee65332c1eccc9a57a755
LAPTOP-IU7VBF35\239003 <8641ba.mmy@gmail.com>
2024-06-20 09:30:59 +0900

    commit

--- S03_2,3/S03_2.php
+++ S03_2,3/S03_2.php
@@ -16,7 +16,7 @@
     </header>
     
     <main>
-        <form action="S03_2_1.php" method="post">
+        <form action="login.php" method="post">
             <div class="form-group">
                 <label for="kokyakuname">&nbsp;顧&nbsp;客&nbsp;名&nbsp;</label> 
                 <input type="text" id="name" name="name" required autofocus placeholder="赤嶺昂太">
@@ -24,74 +24,74 @@
 
             <div class="form-group">
                 <label for="kokyakuname">フリガナ</label>
-                <input type="text" id="name" name="kana" required autofocus placeholder="アカミネコウタ">
+                <input type="text" id="name" name="name" required autofocus placeholder="アカミネコウタ">
             </div> 
 
             <div class="form-group">
                 <label for="sex">&nbsp;性&emsp;&nbsp;別&nbsp;</label>
                 <td>
-                    <input type="radio" name="gender" id="male" value="男性" checked><label for="male">男性&nbsp;&nbsp;</label>
-                    <input type="radio" name="gender" id="female" value="女性"><label for="female">女性&nbsp;&nbsp;</label>
-                    <input type="radio" name="gender" id="sonota" value="その他"><label for="sonota">その他</label>                
+                    <input type="radio" name="sex" id="male" value="男性" checked><label for="male">男性&nbsp;&nbsp;</label>
+                    <input type="radio" name="sex" id="female" value="女性"><label for="female">女性&nbsp;&nbsp;</label>
+                    <input type="radio" name="sex" id="sonota" value="その他"><label for="sonota">その他</label>                
                 </td>
             </div>
 
             <div class="form-group">
                 <label for="date-edit">生年月日</label>
-                <input type="date" name="date" id="date" value="2000-01-01" required>
+                <input type="date" id="date" value="2000-01-01" required>
             </div>
 
             <div class="form-group">
                 <label for="adress">&nbsp;住&emsp;&nbsp;所&nbsp;</label> 
-                <select name="pref">
+                <input type="radio" select name="pref">
                     <option value="">選択してください</option>
-                    <option value="1">北海道</option>
-                    <option value="2">青森県</option>
-                    <option value="3">岩手県</option>
-                    <option value="4">宮城県</option>
-                    <option value="5">秋田県</option>
-                    <option value="6">山形県</option>
-                    <option value="7">福島県</option>
-                    <option value="8">茨城県</option>
-                    <option value="9">栃木県</option>
-                    <option value="10">群馬県</option>
-                    <option value="11">埼玉県</option>
-                    <option value="12">千葉県</option>
-                    <option value="13">東京都</option>
-                    <option value="14">神奈川県</option>
-                    <option value="15">新潟県</option>
-                    <option value="16">富山県</option>
-                    <option value="17">石川県</option>
-                    <option value="18">福井県</option>
-                    <option value="19">山梨県</option>
-                    <option value="20">長野県</option>
-                    <option value="21">岐阜県</option>
-                    <option value="22">静岡県</option>
-                    <option value="23">愛知県</option>
-                    <option value="24">三重県</option>
-                    <option value="25">滋賀県</option>
-                    <option value="26">京都府</option>
-                    <option value="27">大阪府</option>
-                    <option value="28">兵庫県</option>
-                    <option value="29">奈良県</option>
-                    <option value="30">和歌山県</option>
-                    <option value="31">鳥取県</option>
-                    <option value="32">島根県</option>
-                    <option value="33">岡山県</option>
-                    <option value="34">広島県</option>
-                    <option value="35">山口県</option>
-                    <option value="36">徳島県</option>
-                    <option value="37">香川県</option>
-                    <option value="38">愛媛県</option>
-                    <option value="39">高知県</option>
-                    <option value="40">福岡県</option>
-                    <option value="41">佐賀県</option>
-                    <option value="42">長崎県</option>
-                    <option value="43">熊本県</option>
-                    <option value="44" selected>大分県</option>
-                    <option value="45">宮崎県</option>
-                    <option value="46">鹿児島県</option>
-                    <option value="47">沖縄県</option>
+                    <option value="北海道">北海道</option>
+                    <option value="青森県">青森県</option>
+                    <option value="岩手県">岩手県</option>
+                    <option value="宮城県">宮城県</option>
+                    <option value="秋田県">秋田県</option>
+                    <option value="山形県">山形県</option>
+                    <option value="福島県">福島県</option>
+                    <option value="茨城県">茨城県</option>
+                    <option value="栃木県">栃木県</option>
+                    <option value="群馬県">群馬県</option>
+                    <option value="埼玉県">埼玉県</option>
+                    <option value="千葉県">千葉県</option>
+                    <option value="東京都">東京都</option>
+                    <option value="神奈川県">神奈川県</option>
+                    <option value="新潟県">新潟県</option>
+                    <option value="富山県">富山県</option>
+                    <option value="石川県">石川県</option>
+                    <option value="福井県">福井県</option>
+                    <option value="山梨県">山梨県</option>
+                    <option value="長野県">長野県</option>
+                    <option value="岐阜県">岐阜県</option>
+                    <option value="静岡県">静岡県</option>
+                    <option value="愛知県">愛知県</option>
+                    <option value="三重県">三重県</option>
+                    <option value="滋賀県">滋賀県</option>
+                    <option value="京都府">京都府</option>
+                    <option value="大阪府">大阪府</option>
+                    <option value="兵庫県">兵庫県</option>
+                    <option value="奈良県">奈良県</option>
+                    <option value="和歌山県">和歌山県</option>
+                    <option value="鳥取県">鳥取県</option>
+                    <option value="島根県">島根県</option>
+                    <option value="岡山県">岡山県</option>
+                    <option value="広島県">広島県</option>
+                    <option value="山口県">山口県</option>
+                    <option value="徳島県">徳島県</option>
+                    <option value="香川県">香川県</option>
+                    <option value="愛媛県">愛媛県</option>
+                    <option value="高知県">高知県</option>
+                    <option value="福岡県">福岡県</option>
+                    <option value="佐賀県">佐賀県</option>
+                    <option value="長崎県">長崎県</option>
+                    <option value="熊本県">熊本県</option>
+                    <option value="大分県" selected>大分県</option>
+                    <option value="宮崎県">宮崎県</option>
+                    <option value="鹿児島県">鹿児島県</option>
+                    <option value="沖縄県">沖縄県</option>
                     </select>
             </div>
 
