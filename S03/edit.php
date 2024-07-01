fd28ca4697759f9c63f257046e720c6681e9ea37
ivy239001 <c239001@ivy.ac.jp>
2024-07-01 09:24:21 +0900

    b

--- S03/edit.php
+++ S03/edit.php
@@ -45,24 +45,47 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="utf-8">
     <title>顧客編集画面</title>
-    <link rel="stylesheet" type="text/css" href="S03.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap');
+    </style>
+    <link rel="stylesheet" type="text/css" href="edit.css">
 </head>
+
 <body>
-    <h1>顧客編集画面</h1>
-    <form action="update.php" method="post">
-        <input type="hidden" name="Cust_id" value="<?php echo $Cust_id; ?>">
-        <label for="Name">顧客名:</label>
-        <input type="text" id="Name" name="Name" value="<?php echo $Name; ?>"><br>
-        <label for="State_id">都道府県ID:</label>
-        <input type="text" id="State_id" name="State_id" value="<?php echo $State_id; ?>"><br>
-        <label for="Gender">性別:</label>
-        <input type="text" id="Gender" name="Gender" value="<?php echo $Gender; ?>"><br>
-        <label for="Birth_day">生年月日:</label>
-        <input type="text" id="Birth_day" name="Birth_day" value="<?php echo $Birth_day; ?>"><br>
-        <input type="submit" value="更新">
-    </form>
+    <header>
+        <img src="../graphic/ニトリロゴ.jpg" alt="Logo" class="logo">
+        <p>顧客編集画面</p>
+    </header>
+
+    <main>
+
+        <form action="update.php" method="post">
+            <input type="hidden" name="Cust_id" value="<?php echo $Cust_id; ?>">
+            <label for="Name">顧客名:</label>
+            <input type="text" id="Name" name="Name" value="<?php echo $Name; ?>"><br>
+            <label for="State_id">都道府県ID:</label>
+            <input type="text" id="State_id" name="State_id" value="<?php echo $State_id; ?>"><br>
+            <label for="Gender">性別:</label>
+            <input type="text" id="Gender" name="Gender" value="<?php echo $Gender; ?>"><br>
+            <label for="Birth_day">生年月日:</label>
+            <input type="text" id="Birth_day" name="Birth_day" value="<?php echo $Birth_day; ?>"><br>
+            <input type="submit" value="更新"> 
+        </form>
+
+    </main>
+
+    <button onclick="location.href='S03.php'" type="button" name="name" value="value" id="BackButton">戻る</button>
+
+    <footer>
+        © 2024 <a href="https://www.ivy.ac.jp/">アイビクション</a>
+        <div class="back">
+        </div>
+
+    </footer>
 </body>
+
 </html>
