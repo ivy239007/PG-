63a6d319cbe37204c52f1fa80c24ced55274b36f
LAPTOP-IU7VBF35\239003 <8641ba.mmy@gmail.com>
2024-07-01 10:01:59 +0900

    2_1 デザイン修正　2_2 CSS作成

--- S03_2,3/S03_2_1.php
+++ S03_2,3/S03_2_1.php
@@ -23,33 +23,48 @@ $_SESSION['addres'] = $addres;
 $_SESSION['tel'] = $tel;
 $_SESSION['email'] = $email;
 ?>
+
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<title>DBに接続しているフォーム｜確認画面</title>
-
-<link rel="stylesheet" type="text/css" href="stylesS03_2_1.css"> 
+    <meta charset="utf-8">
+    <title>登録確認画面</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap');
+    </style>
+    <link rel="stylesheet" type="text/css" href="stylesS03_2_1.css"> 
 </head>
 
 <body>
-<div class="container">
-<h1>確認画面</h1>
-<form action="S03_2_2.php" method="post">
-<dl>
-<dt>お名前</dt><dd><?php echo $name ?></dd>
-<dt>カナ</dt><dd><?php echo $kana ?></dd>
-<dt>性別</dt><dd><?php echo $gender ?></dd>
-<dt>生年月日</dt><dd><?php echo $date ?></dd>
-<dt>住所</dt><dd><?php echo $addres ?></dd>
-<dt>電話番号</dt><dd><?php echo $tel ?></dd>
-<dt>メールアドレス</dt><dd><?php echo $email ?></dd> 
-</dl>
-<p>
-<input type="submit" value="送信">
-<input type="button" value="戻る" onclick="history.back();">
-</p>
-</form>
-</div>
+    <header>
+        <img src="../graphic/ニトリロゴ.jpg" alt="Logo" class="logo"> 
+        <p>登録確認画面</p>
+    </header>
+    <main>
+        <form action="S03_2_2.php">
+            <div class="container">
+                <form action="S03_2_2.php" method="post">
+                <dl>
+                <dt>お名前</dt><dd><?php echo $name ?></dd>
+                <dt>カナ</dt><dd><?php echo $kana ?></dd>
+                <dt>性別</dt><dd><?php echo $gender ?></dd>
+                <dt>生年月日</dt><dd><?php echo $date ?></dd>
+                <dt>住所</dt><dd><?php echo $addres ?></dd>
+                <dt>電話番号</dt><dd><?php echo $tel ?></dd>
+                <dt>メールアドレス</dt><dd><?php echo $email ?></dd> 
+                </dl>
+            </div>
+
+            <div class="Btn">
+                <input type="submit" value="完了">
+            </div>
+        </form>
+        <button onclick="location.href='S03_2.php'" type="button" name="name" value="value" id="BackButton">戻る</button>
+    </main>
+    <footer>
+        © 2024 <a href="https://www.ivy.ac.jp/">アイビクション</a>
+        <div class="back">
+        </div>
+    </footer>
 </body>
 </html>
