53ae6d41fbfb61993f469679c3f49c0338e8119f
239007 <c239007@ivy.ac.jp>
2024-07-03 14:41:56 +0900

    変更画面の変更

--- S03/edit.php
+++ S03/edit.php
@@ -35,6 +35,162 @@ if ($result->num_rows > 0) {
     $State_id = $row['State_id'];
     $Gender = $row['Gender'];
     $Birth_day = $row['Birth_day'];
+
+    switch($State_id){
+        case "1":
+            $State_id = "北海道";
+            break;
+        case "2":
+            $State_id = "青森県";
+            break;
+        case "3":
+            $State_id = "岩手県";
+            break;
+        case "4":
+            $State_id = "宮城県";
+            break;
+        case "5":
+            $State_id = "秋田県";
+            break;
+        case "6":
+            $State_id = "山形県";
+            break;
+        case "7":
+            $State_id = "福島県";
+            break;
+        case "8":
+            $State_id = "茨城県";
+            break;
+        case "9":
+            $State_id = "栃木県";
+            break;
+        case "10":
+            $State_id = "群馬県";
+            break;
+        case "11":
+            $State_id = "埼玉県";
+            break;
+        case "12":
+            $State_id = "千葉県";
+            break;
+        case "13":
+            $State_id = "東京都";
+            break;
+        case "14":
+            $State_id = "神奈川県";
+            break;
+        case "15":
+            $State_id = "新潟県";
+            break;
+        case "16":
+            $State_id = "富山県";
+            break;
+        case "17":
+            $State_id = "石川県";
+            break;
+        case "18":
+            $State_id = "福井県";
+            break;
+        case "19":
+            $State_id = "山梨県";
+            break;
+        case "20":
+            $State_id = "長野県";
+            break;
+        case "21":
+            $State_id = "岐阜県";
+            break;
+        case "22":
+            $State_id = "静岡県";
+            break;
+        case "23":
+            $State_id = "愛知県";
+            break;
+        case "24":
+            $State_id = "三重県";
+            break;
+        case "25":
+            $State_id = "滋賀県";
+            break;
+        case "26":
+            $State_id = "京都府";
+            break;
+        case "27":
+            $State_id = "大阪府";
+            break;
+        case "28":
+            $State_id = "兵庫県";
+            break;
+        case "29":
+            $State_id = "奈良県";
+            break;
+        case "30":
+            $State_id = "和歌山県";
+            break;
+        case "31":
+            $State_id = "鳥取県";
+            break;
+        case "32":
+            $State_id = "島根県";
+            break;
+        case "33":
+            $State_id = "岡山県";
+            break;
+        case "34":
+            $State_id = "広島県";
+            break;
+        case "35":
+            $State_id = "山口県";
+            break;
+        case "36":
+            $State_id = "徳島県";
+            break;
+        case "37":
+            $State_id = "香川県";
+            break;
+        case "38":
+            $State_id = "愛媛県";
+            break;
+        case "39":
+            $State_id = "高知県";
+            break;
+        case "40":
+            $State_id = "福岡県";
+            break;
+        case "41":
+            $State_id = "佐賀県";
+            break;
+        case "42":
+            $State_id = "長崎県";
+            break;
+        case "43":
+            $State_id = "熊本県";
+            break;
+        case "44":
+            $State_id = "大分県";
+            break;
+        case "45":
+            $State_id = "宮崎県";
+            break;
+        case "46":
+            $State_id = "鹿児島県";
+            break;
+        case "47":
+            $State_id = "沖縄県";
+            break;
+    }
+
+    switch($Gender){
+        case "1":
+            $Gender = "男性";
+            break;
+        case "2":
+            $Gender = "女性";
+            break;
+        case "3":
+            $Gender = "その他";
+            break;
+    }
 } else {
     echo "No records found.";
     exit();
@@ -68,11 +224,13 @@ $conn->close();
             <label for="Name">顧客名:</label>
             <input type="text" id="Name" name="Name" value="<?php echo $Name; ?>"><br>
             <label for="State_id">都道府県ID:</label>
-            <input type="text" id="State_id" name="State_id" value="<?php echo $State_id; ?>"><br>
+            <input type="text" id="State_id" name="State_id" value="<?php echo $State_id; ?>">
+            <p>〇〇県、〇〇府、〇〇都まで入力してください。</p>
             <label for="Gender">性別:</label>
             <input type="text" id="Gender" name="Gender" value="<?php echo $Gender; ?>"><br>
             <label for="Birth_day">生年月日:</label>
-            <input type="text" id="Birth_day" name="Birth_day" value="<?php echo $Birth_day; ?>"><br>
+            <input type="text" id="Birth_day" name="Birth_day" value="<?php echo $Birth_day; ?>">
+            <p>ハイフン（-）を抜いて入力してください。</p>
             <input type="submit" value="更新"> 
         </form>
 
--- S03/update.php
+++ S03/update.php
@@ -29,6 +29,162 @@ $Gender = $_POST['Gender'];
 $Birth_day = $_POST['Birth_day'];
 $Birth_day  = (String)$Birth_day;
 
+switch($State_id){
+    case "北海道":
+        $State_id = 1;
+        break;
+    case "青森県":
+        $State_id = 2;
+        break;
+    case "岩手県":
+        $State_id = 3;
+        break;
+    case "宮城県":
+        $State_id = 4;
+        break;
+    case "秋田県":
+        $State_id = 5;
+        break;
+    case "山形県":
+        $State_id = 6;
+        break;
+    case "福島県":
+        $State_id = 7;
+        break;
+    case "茨城県":
+        $State_id = 8;
+        break;
+    case "栃木県":
+        $State_id = 9;
+        break;
+    case "群馬県":
+        $State_id = 10;
+        break;
+    case "埼玉県":
+        $State_id = 11;
+        break;
+    case "千葉県":
+        $State_id = 12;
+        break;
+    case "東京都":
+        $State_id = 13;
+        break;
+    case "神奈川県":
+        $State_id = 14;
+        break;
+    case "新潟県":
+        $State_id = 15;
+        break;
+    case "富山県":
+        $State_id = 16;
+            break;
+    case "石川県":
+        $State_id = 17;
+        break;
+    case "福井県":
+        $State_id = 18;
+        break;
+    case "山梨県":
+        $State_id = 19;
+        break;
+    case "長野県":
+        $State_id = 20;
+        break;
+    case "岐阜県":
+        $State_id = 21;
+        break;
+    case "静岡県":
+        $State_id = 22;
+        break;
+    case "愛知県":
+        $State_id = 23;
+        break;
+    case "三重県":
+        $State_id = 24;
+        break;
+    case "滋賀県":
+        $State_id = 25;
+        break;
+    case "京都府":
+        $State_id = 26;
+        break;
+    case "大阪府":
+        $State_id = 27;
+        break;
+    case "兵庫県":
+        $State_id = 28;
+        break;
+    case "奈良県":
+        $State_id = 29;
+        break;
+    case "和歌山県":
+        $State_id = 30;
+        break;
+    case "鳥取県":
+        $State_id = 31;
+        break;
+    case "島根県":
+        $State_id = 32;
+        break;
+    case "岡山県":
+        $State_id = 33;
+        break;
+    case "広島県":
+        $State_id = 34;
+        break;
+    case "山口県":
+        $State_id = 35;
+        break;
+    case "徳島県":
+        $State_id = 36;
+        break;
+    case "香川県":
+        $State_id = 37;
+        break;
+    case "愛媛県":
+        $State_id = 38;
+        break;
+    case "高知県":
+        $State_id = 39;
+        break;
+    case "福岡県":
+        $State_id = 40;
+        break;
+    case "佐賀県":
+        $State_id = 41;
+        break;
+    case "長崎県":
+        $State_id = 42;
+        break;
+    case "熊本県":
+        $State_id = 43;
+        break;
+    case "大分県":
+        $State_id = 44;
+        break;
+    case "宮崎県":
+        $State_id = 45;
+        break;
+    case "鹿児島県":
+        $State_id = 46;
+        break;
+    case "沖縄県":
+        $State_id = 47;
+        break;
+}
+
+switch($Gender){
+    case "男性";
+        $Gender = "1";
+        break;
+    case "女性":
+        $Gender = "2";
+        break;
+    case "その他":
+        $Gender = "3";
+        break;
+}
+
 $sql = "UPDATE customers SET Name=?, State_id=?, Gender=?, Birth_day=? WHERE Cust_id=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sisis", $Name, $State_id, $Gender, $Birth_day, $Cust_id);
