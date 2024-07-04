78af16820b09df825e47d5b4b86690b0ba9b4b43
LAPTOP-IU7VBF35\239003 <8641ba.mmy@gmail.com>
2024-07-04 13:39:22 +0900

    S04_2_1 CSS完了

--- S04_2,3/S04_2_1.php
+++ S04_2,3/S04_2_1.php
@@ -27,28 +27,42 @@ $_SESSION['qref'] = $qref;
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<title>DBに接続しているフォーム｜確認画面</title>
-<link rel="stylesheet" href="css/form.css">
+    <meta charset="utf-8">
+    <title>登録確認画面</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap');
+    </style>
+    <link rel="stylesheet" type="text/css" href="stylesS04_2_1.css"> 
 </head>
 
 <body>
-<div class="container">
-<h1>確認画面</h1>
-<form action="S04_2_2.php" method="post">
-<dl>
-<dt>書籍名</dt><dd><?php echo $book_title ?></dd>
-<dt>著作者</dt><dd><?php echo $author_name ?></dd>
-<dt>値段</dt><dd><?php echo $price ?></dd>
-<dt>刊行日</dt><dd><?php echo $publication_date ?></dd>
-<dt>出版社</dt><dd><?php echo $pref ?></dd>
-<dt>分類</dt><dd><?php echo $qref ?></dd> 
-</dl>
-<p>
-<input type="submit" value="送信">
-<input type="button" value="戻る" onclick="history.back();">
-</p>
-</form>
-</div>
+    <header>
+        <img src="../graphic/ニトリロゴ.jpg" alt="Logo" class="logo"> 
+        <p>登録確認画面</p>
+    </header>
+    <main>
+        <form action="S04_2_2.php" method="post">
+            <div class="container">
+                <form action="S03_2_2.php" method="post">
+                <dl>
+                <dt>書籍名</dt><dd><?php echo $book_title ?></dd>
+                <dt>著作者</dt><dd><?php echo $author_name ?></dd>
+                <dt>値段</dt><dd><?php echo $price ?></dd>
+                <dt>刊行日</dt><dd><?php echo $publication_date ?></dd>
+                <dt>出版社</dt><dd><?php echo $pref ?></dd>
+                <dt>分類</dt><dd><?php echo $qref ?></dd> 
+                </dl>
+            </div>
+
+            <div class="Btn">
+                <input type="submit" value="完了">
+            </div>
+        </form>
+        <button onclick="location.href='S04_2.php'" type="button" name="name" value="value" id="BackButton">戻る</button>
+    </main>
+    <footer>
+        © 2024 <a href="https://www.ivy.ac.jp/">アイビクション</a>
+        <div class="back"></div>
+    </footer>
 </body>
 </html>
